2cf3ee45c048e2c6d54b7ed7e4a57b4c72b80b44
Jacob Hardman <42230736+thejacobhardman@users.noreply.github.com>
2019-10-20 22:44:43 -0600

    finish assignment

--- Match-Making/Match-Making.cpp
+++ Match-Making/Match-Making.cpp
@@ -12,53 +12,173 @@ using namespace std;
 int main()
 {
 	ifstream inputFile;
-	int maxHeight, minHeight, age, maxAge, minAge;
-	string name, eyeColor;
-	char gender, smoker;
+	int height, maxHeight, minHeight, age, maxAge, minAge;
+	string name, eyeColor, preferredEyeColor, userInput;
+	char gender, preferredGender, smoker, preferredSmoker;
+	bool isRunning = true, genderMatch = false, validGender = false, validHeight = false, validAge = false, validEyeColor = false, validSmoker = false;
 
-	// Open the file.
-	inputFile.open("database.txt");
+	while (isRunning) {
 
-	// If the file successfully opened, process it.
-	if (inputFile)
-	{
-		/*cout << "Enter the gender of your ideal match: ";
-		cin >> gender;
-		cout << "Enter the maximum age of your preferred match: ";
-		cin >> maxAge;
-		cout << "Enter the minimum age of your preffered match: ";
-		cin >> minAge;
-		cout << "Enter the minimum height in inches: ";
-		cin >> minHeight;
-		cout << "Enter the maximum height in inches: ";
-		cin >> maxHeight;
-		cout << "Enter your preferred eye color: ";
-		cin >> eyeColor;
-		cout << "Enter your smoking preference (Y/N): ";
-		cin >> smoker;*/
+		int records = 0, matches = 0, partialMatches = 0, perfectMatches = 0;
 
-		cout << "Enter the maximum age: ";
-		cin >> maxAge;
-		cout << "Enter the minimum age: ";
-		cin >> minAge;
+		inputFile.open("Program3.dat");
 
-		while (inputFile >> name >> age)
+		if (inputFile)
 		{
-			//cout <<  name << " " << age << " "<< smoker << endl;
-			if (age >= minAge && age <= maxAge)
-			{
-				cout << name << " is within the age range you specified he/she is "
-					<< age << endl;
-			}
-		}
+			while (validGender == false) {
+				cout << "Enter the preferred gender of your ideal match (M/F): ";
+				cin >> preferredGender;
 
-		// Close the file.
-		inputFile.close();
+				if (preferredGender == 'M' || preferredGender == 'F') {
+					break;
+				}
+				else {
+					cout << "Please select a valid gender by entering a capital letter." << endl;
+				}
+			}
+			while (validHeight == false) {
+				cout << "Enter the minimum height in inches: ";
+				cin >> minHeight;
+
+				if (minHeight <= 0) {
+					cout << "Please enter a valid height." << endl;
+				}
+				else {
+					break;
+				}
+			}
+			while (validHeight == false) {
+				cout << "Enter the maximum height in inches: ";
+				cin >> maxHeight;
+
+				if (maxHeight <= 0) {
+					cout << "Please enter a valid height." << endl;
+				}
+				else {
+					break;
+				}
+			}
+			while (validAge == false) {
+				cout << "Enter the minimum age: ";
+				cin >> minAge;
+
+				if (minAge <= 0) {
+					cout << "Please enter a valid age." << endl;
+				}
+				else if (minAge < 18) {
+					cout << "Enter a valid LEGAL age. (geez...)" << endl;
+				}
+				else {
+					break;
+				}
+			}
+			while (validAge == false) {
+				cout << "Enter the maximum age: ";
+				cin >> maxAge;
+
+				if (maxAge <= 0) {
+					cout << "Please enter a valid age." << endl;
+				}
+				else if (maxAge < 18) {
+					cout << "Enter a valid LEGAL age. (geez...)" << endl;
+				}
+				else if (maxAge > 100) {
+					cout << "Hey who am I to judge?" << endl;
+					break;
+				}
+				else {
+					break;
+				}
+			}
+			while (validEyeColor == false) {
+				cout << "Enter your preferred eye color: ";
+				cin >> preferredEyeColor;
+
+				if (preferredEyeColor == "Blue" || preferredEyeColor == "Grey" || preferredEyeColor == "Green" || preferredEyeColor == "Brown") {
+					break;
+				}
+				else {
+					cout << "Please enter a valid eye color with the first letter capitalized." << endl;
+				}
+			}
+			while (validSmoker == false) {
+				cout << "Enter your smoking preference (Y/N): ";
+				cin >> preferredSmoker;
+
+				if (preferredSmoker == 'Y' || preferredSmoker == 'N') {
+					break;
+				}
+				else {
+					cout << "Please select a valid smoking preference by entering a capital letter." << endl;
+				}
+			}
+			cout << endl;
+
+			while (inputFile >> name >> height >> age >> gender >> eyeColor >> smoker)
+			{
+				matches = 0;
+				genderMatch = false;
+				records += 1;
+
+				if (height >= minHeight && height <= maxHeight) {
+					matches += 1;
+				}
+				if (age >= minAge && age <= maxAge)
+				{
+					matches += 1;
+				}
+				if (preferredGender == gender) {
+					matches += 1;
+					genderMatch = true;
+				}
+				if (preferredEyeColor == eyeColor) {
+					matches += 1;
+				}
+				if (preferredSmoker == smoker) {
+					matches += 1;
+				}
+
+				if (matches == 5) {
+					perfectMatches += 1;
+					cout << name << " is a perfect match!" << endl;
+				}
+				else if (matches < 5 && matches > 0 && genderMatch == true) {
+					partialMatches += 1;
+					cout << name << " is a partial match!" << endl;
+				}
+			}
+
+			if (perfectMatches > 0 || partialMatches > 0) {
+				cout << "\nThere were " << perfectMatches << " perfect matches and " << partialMatches << " partial matches out of " << records << " records." << endl;
+			}
+			else {
+				cout << "\nNo matches found out of " << records << " records.";
+			}
+
+			bool userConfirm = false;
+			while (userConfirm == false) {
+				cout << "Would you like to run the program again? (Y/N): ";
+				cin >> userInput;
+
+				if (userInput == "Y" || userInput == "y") {
+					cout << endl;
+					userConfirm = true;
+				}
+				else if (userInput == "N" || userInput == "n") {
+					isRunning = false;
+					userConfirm = true;
+				}
+				else {
+					cout << "Please enter a valid selection." << endl;
+				}
+			}
+
+			inputFile.close();
+		}
+		else
+		{
+			cout << "Error opening the file.\nPlease make sure that the 'Program3.dat' file is located in the same folder as the 'Match-Making.cpp' file.";
+			isRunning = false;
+		}
 	}
-	else
-	{
-		// Display an error message.
-		cout << "Error opening the file.\n";
-	}
-	return 0;
 }
